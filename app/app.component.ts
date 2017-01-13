419ba3a7df8100ba12f72555f0a50371f798d412
Waldemar Bira <waldek.bira@gmail.com>
2017-01-13 20:16:46 +0100

    Added handler for selecting heroes on click

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,18 +24,22 @@ const HEROES: Hero[] = [
     <h1>Hello {{title}}</h1>
     <h2>My heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" 
+        [class.selected]="hero===selectedHero"
+        (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div>
-      <label>id: </label>
-      {{hero.id}}
-    </div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div>
+        <label>id: </label>
+        {{selectedHero.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
     </div>
     `,
   styles: [`
@@ -93,9 +97,10 @@ const HEROES: Hero[] = [
 export class AppComponent {
   heroes = HEROES;
   title = "Tour of Heroes";
-  hero: Hero = {
-    id: 1,
-    name: "Windstrom"
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
 
