3570c9cc149e8fceafd551a3af789b8b52fb96dc
Waldemar Bira <waldek.bira@gmail.com>
2017-01-14 16:44:53 +0100

    Registered hero-detail in appmodule, add @input decorator and use hero-detail in AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,6 +26,7 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
   styles: [`
   .selected {
