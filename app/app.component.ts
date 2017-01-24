a7e3ac97af327beff199a8faafa129a90bfdff9a
Waldemar Bira <waldek.bira@gmail.com>
2017-01-24 23:59:45 +0100

    Created basic dashboard component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-      <h1>Hello {{title}}</h1>
-      <my-heroes></my-heroes>
-      `
+   <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+   `
 })
+
 export class AppComponent {
   title = "Tour of Heroes";
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-dettail.component'
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   declarations: [
-    AppComponent, HeroesComponent, HeroDetailComponent
+    AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
